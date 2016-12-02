3f24ee3d34170f5002f9cc378dbd70007271a347
Amer <amashamdan@gmail.com>
2016-12-01 18:58:13 -0800

    Feat: Upon trying to login, the password and email entered are.
    When the user submits login form, email and password entered are checked.
    If things are good, user will be redirected to homepage.
    If not, message passed to login.ejs which is rendered again.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var ejs = require("ejs");
 var passport = require("passport");
 var bodyParser = require("body-parser");
 var parser = bodyParser.urlencoded({extended: false});
+var bcrypt = require("bcryptjs");
 
 var app = express();
 app.use(secure);
@@ -21,14 +22,35 @@ MongoClient.connect(mongoUrl, function(err, db) {
 	} else {
 		var users = db.collection("users");
 		var message = undefined;
+		var messageType = undefined;
 
 		app.get("/", function(req, res) {
 			res.render("index.ejs");
 		});
 
 		app.get("/login", function(req, res) {
-			res.render("login.ejs", {"message": message});
+			res.render("login.ejs", {"message": message, "messageType": messageType});
 			message = undefined;
+		});
+
+		app.post("/login", parser, function(req, res) {
+			users.find({"email": req.body.email}).toArray(function(err, result) {
+				if (result.length == 0) {
+					message = "Email not found in database!";
+					messageType = "error";
+					res.redirect("/login");
+				} else {
+					bcrypt.compare(req.body.password, result[0].password, function(err, checkResult) {
+					    if (checkResult) {
+					    	res.redirect("/");
+					    } else {
+					    	message = "Incorrect password.";
+					    	messageType = "error";
+					    	res.redirect("/login");
+					    }
+					});
+				}
+			});
 		});
 
 		app.get("/signup", function(req, res) {
@@ -42,18 +64,23 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		});
 
 		app.post("/signup", parser, function(req, res) {
-			users.insert({
-				"name": req.body.name,
-				"email": req.body.email,
-				"password": req.body.password1,
-				"address": req.body.address,
-				"city": req.body.city,
-				"state": req.body.state,
-				"zip": req.body.zip
-			}, function() {
-				message = "Thank you for registering. Now you can login to start using the Vault.";
-				res.redirect("/login");
-			})
+			bcrypt.genSalt(10, function(err, salt) {
+			    bcrypt.hash(req.body.password1, salt, function(err, hash) {
+					users.insert({
+						"name": req.body.name,
+						"email": req.body.email,
+						"password": hash,
+						"address": req.body.address,
+						"city": req.body.city,
+						"state": req.body.state,
+						"zip": req.body.zip
+					}, function() {
+						message = "Thank you for registering. Now you can login to start using the Vault.";
+						messageType = "success";
+						res.redirect("/login");
+					})
+			    });
+			});
 		});
 	}
 });
