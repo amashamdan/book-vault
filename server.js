d7c2bcb97d521613e98bd76aafa5aebc109956d5
Amer Hamdan <amashamdan@gmail.com>
2016-12-02 17:27:10 -0800

    Feat: Login session and crsf now implemented.
    Login session now enabled using client-sessions module.
    csrf also implemented to prevent cross site request forging.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require("express");
 var secure = require('express-force-https');
 var mongodb = require("mongodb");
 var ejs = require("ejs");
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
 var bodyParser = require("body-parser");
 var parser = bodyParser.urlencoded({extended: false});
 var bcrypt = require("bcryptjs");
+var session = require("client-sessions");
+var csrf = require('csurf');
 
 var app = express();
 app.use(secure);
@@ -17,6 +17,20 @@ var mongoUrl = process.env.BOOKS;
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
 app.use("/scripts", express.static(__dirname + "/views/scripts"));
 
+app.use(session({
+	cookieName: "session",
+	secret: "sdjfngsluifndzljbgsjlgbns",
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+	httpOnly: true, //Don't let browser javascript access cookies.
+	secure: true, //only use cookies over https
+	ephemeral: true //Delete this cookie when the broswer is closed.
+}));
+
+/* THIS MUST LIKE THIS IN THIS ORDER */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(csrf());
+
 MongoClient.connect(mongoUrl, function(err, db) {
 	if (err) {
 		res.end("Failed to connect to database.");
@@ -24,39 +38,46 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		var users = db.collection("users");
 
 		app.get("/", function(req, res) {
-			res.render("index.ejs", {"user": undefined});
+			if (req.session.user) {
+				res.render("index.ejs", {"user": req.session.user});				
+			} else {
+				res.render("index.ejs", {"user": undefined});
+			}
 		});
 
 		app.get("/login", function(req, res) {
-			res.render("login.ejs", {"message": undefined, "messageType": undefined});
+			res.render("login.ejs", {"csrfToken": req.csrfToken(), "message": undefined, "messageType": undefined});
 		});
-
-		/*app.post("/login", passport.authenticate("local"), function(req, res) {
-			res.render("index.ejs", {"user": req.user});
-		});*/
 
 		app.post("/login", parser, function(req, res) {
 			users.find({"email": req.body.email}).toArray(function(err, result) {
 				if (result.length == 0) {
 					var message = "Email not found in database!";
 					var messageType = "error";
-					res.render("login.ejs", {"message": message, "messageType": messageType});
+					res.render("login.ejs", {"csrfToken": req.csrfToken(), "message": message, "messageType": messageType});
 				} else {
 					bcrypt.compare(req.body.password, result[0].password, function(err, checkResult) {
 					    if (checkResult) {
+					    	req.session.user = result[0];
+					    	delete req.session.user.password;
 					    	res.redirect("/");
 					    } else {
 					    	var message = "Incorrect password.";
 					    	var messageType = "error";
-					    	res.render("login.ejs", {"message": message, "messageType": messageType});
+					    	res.render("login.ejs", {"csrfToken": req.csrfToken(), "message": message, "messageType": messageType});
 					    }
 					});
 				}
 			});
 		});
 
+		app.get("/logout", function(req, res) {
+			req.session.reset();
+			res.redirect("/");
+		});
+
 		app.get("/signup", function(req, res) {
-			res.render("register.ejs", {"message": undefined});
+			res.render("register.ejs", {"csrfToken": req.csrfToken(), "message": undefined});
 		});
 
 		app.post("/signup", parser, function(req, res) {
@@ -69,7 +90,7 @@ MongoClient.connect(mongoUrl, function(err, db) {
 					}
 				}
 				if (found) {
-					res.render("register.ejs", {"message": "Email already registered."})
+					res.render("register.ejs", {"csrfToken": req.csrfToken(), "message": "Email already registered."})
 				} else {
 					bcrypt.genSalt(10, function(err, salt) {
 					    bcrypt.hash(req.body.password1, salt, function(err, hash) {
