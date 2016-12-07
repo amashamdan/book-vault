147459cde52f481bdb4c4922a4068c6adf82c61e
Amer Hamdan <amashamdan@gmail.com>
2016-12-06 17:31:19 -0800

    Refactor and Feat: Added changeLayout function and handler for password change form.
    changeLayout form is called by both handlers of 'Edit' button and 'Change password' button.
    It replaces the contents with the desired form.
    The submit handler for the password change form also checks for valid choice of password.

--- views/scripts/profile.js
+++ views/scripts/profile.js
@@ -1,28 +1,74 @@
 $(document).ready(function() {
 	$("#edit").click(function() {
-		$(".start").children().slideToggle(function() {
-			$(".start").children().remove();
-			$(".start").css({"display": "none"});
-			$(".start").append(
-				'<div class="container">' +
-				"<form class='update-form' action='/update' method='post'>" + 
-				'<input type="hidden" name="_csrf" value=' + csrfToken + '>' +
-				'<p class="info-header"><strong>Name:</strong></p>' + 
-				'<input type="text" name="name" value="' + name + '">' + 
-				'<p class="info-header"><strong>Address:</strong></p>' + 
-				'<input type="text" name="address" value="' + address + '"><br/>' + 
-				'<input type="text" name="city" value="' + city + '"><br/>' +
-				'<input type="text" name="state" value="' + state + '"><br/>' +
-				'<input type="text" name="zip" value="' + zip + '"><br/>' + 
-				'<button class="generic-button" type="submit">Submit</button>' + 
-				'<button class="generic-button" id="cancel">Cancel</button>' + 
-				'</form></div>'
-				);
-			$(".start").slideToggle();
-			$("#cancel").click(function(e) {
-				e.preventDefault();
-				location.reload();
-			});	
-		});
+		changeLayout("update-info");
+	});
+
+	$("#change-password").click(function() {
+		changeLayout("update-password");
 	});
 });
+
+function changeLayout(caller) {
+	if (caller == "update-info") {
+		var newContent = '<div class="container">' +
+			"<form class='update-form' action='/update' method='post'>" + 
+			'<input type="hidden" name="_csrf" value=' + csrfToken + '>' +
+			'<p class="info-header"><strong>Name:</strong></p>' + 
+			'<input type="text" name="name" value="' + name + '">' + 
+			'<p class="info-header"><strong>Address:</strong></p>' + 
+			'<input type="text" name="address" value="' + address + '"><br/>' + 
+			'<input type="text" name="city" value="' + city + '"><br/>' +
+			'<input type="text" name="state" value="' + state + '"><br/>' +
+			'<input type="text" name="zip" value="' + zip + '"><br/>' + 
+			'<button class="generic-button" type="submit">Submit</button>' + 
+			'<button class="generic-button" id="cancel">Cancel</button>' + 
+			'</form></div>'
+	} else if (caller == "update-password") {
+		var newContent = '<div class="container">' +
+			"<form class='update-form' id='password-update-form' action='/updatePassword' method='post'>" + 
+			'<input type="hidden" name="_csrf" value=' + csrfToken + '>' +
+			'<p class="info-header"><strong>New password:</strong><span style="font-size: 0.7em">(8 charachters minimum including a capital letter, number and a special character)</span></p>' + 
+			'<input id="pass1" type="password" name="password1">' + 
+			'<p class="info-header"><strong>Confirm new password:</strong></p>' + 
+			'<input id="pass2" type="password" name="password2"><br/>' + 
+			'<button class="generic-button" type="submit">Submit</button>' + 
+			'<button class="generic-button" id="cancel">Cancel</button>' + 
+			'</form></div>'
+	}
+
+	$(".start").children().slideToggle(function() {
+		$(".start").children().remove();
+		$(".start").css({"display": "none"});
+		$(".start").append(newContent);
+		$(".start").slideToggle();
+		// The following two handlers should be here since they cannot be run before they are appended.
+		$("#cancel").click(function(e) {
+			e.preventDefault();
+			location.reload();
+		});	
+		$("#password-update-form").submit(function(e) {
+			e.preventDefault();
+			$(".error-message").remove();
+			var errorFound = false;
+			if ($("#pass1").val().length < 8) {
+				$("#password-update-form").prepend("<p class='error-message'>Password is not 8 or more characters.</p>");
+				errorFound = true;
+			}else if (!$("#pass1").val().match(/[A-Z]/g)) {
+				$("#password-update-form").prepend("<p class='error-message'>Password doesn't contain a capital letter.</p>");
+				errorFound = true;
+			}else if (!$("#pass1").val().match(/[0-9]/g)) {
+				$("#password-update-form").prepend("<p class='error-message'>Password doesn't contain a number.</p>");
+				errorFound = true;
+			}else if (!$("#pass1").val().match(/[!@#$%^&*]/g)) {
+				$("#password-update-form").prepend("<p class='error-message'>Password doesn't contain a special character.</p>");
+				errorFound = true;
+			} else if ($("#pass1").val() !== $("#pass2").val()) {
+				$("#password-update-form").prepend("<p class='error-message'>Passwords do not match.</p>");
+				errorFound = true;
+			}
+			if (!errorFound) {
+				e.target.submit();
+			}
+		});
+	});
+}
