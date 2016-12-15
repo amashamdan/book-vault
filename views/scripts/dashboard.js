871b7dcc7e0f6e70426d55a7359b96fea004e8cd
Amer Hamdan <amashamdan@gmail.com>
2016-12-15 14:02:45 -0800

    Refactor: Created new function showBooks.
    The contents of the Ajax callback function were moved into this new function.

--- views/scripts/dashboard.js
+++ views/scripts/dashboard.js
@@ -14,88 +14,7 @@ $(document).ready(function() {
 		$.ajax({
 			url: link,
 			success: function(results) {
-				if (results.totalItems == 0) {
-					alert("No results found.");
-					$("#search-results").fadeOut();
-				} else {
-					$("#search-results").children().remove();
-					if (results.items.length < 10) {
-						var limit = results.items.length;
-					} else {
-						var limit = 10;
-					}
-					for (var i = 0; i < limit; i++) {
-						var title = results.items[i].volumeInfo.title;
-						if (results.items[i].volumeInfo.subtitle) {
-							title = title + ": " + results.items[i].volumeInfo.subtitle;
-						}
-						var authors = results.items[i].volumeInfo.authors;
-						var link = results.items[i].volumeInfo.canonicalVolumeLink;
-						var categories = results.items[i].volumeInfo.categories;
-						var description = results.items[i].volumeInfo.description;
-						var image = results.items[i].volumeInfo.imageLinks.thumbnail;
-						var pages = results.items[i].volumeInfo.pageCount;
-						if (results.items[i].volumeInfo.industryIdentifiers) {
-							var isbn = results.items[i].volumeInfo.industryIdentifiers[0].identifier;
-						} else {
-							continue
-						}
-						
-						$("#search-results").append('<div class="result">' + 
-							'<input type="hidden" class="isbn" value=' + isbn + '>' +
-							'<p class="book-title">' + title + '</p>' +
-							//MUST USE FORM FOR csrfToken. When ajax uses post, server expects form data and hence csfrToken. Token passed in ajax post request below.
-							'<form class="addition" method="post" action="/addbook"><button type="submit" class="add-this-book">Add this book</button><a href=' + link + ' target="blank"><button class="generic-button more-info">More info</button></a><button class="cancel-button">Cancel</button></form>' +
-							'<div class="other-information">' + 
-							'<div class="image-wrapper"><img class="result-image" src=' + image + '></div>' +
-							'<div class="info-wrapper">' +
-							'<p class="result-authors"><strong>Authors:</strong> <span>' + authors + '</span></p>' +
-							'<p class="result-categotries"><strong>Categories:</strong> <span>' + categories + '</span></p>' +
-							'<p class="result-pages"><strong>Pages:</strong> <span>' + pages + '</span></p>' +
-							'<p class="result-description"><strong>Descrition:</strong> <span>' + description + '</span></p>' +
-							'</div>' +
-							'</div>' +
-							'</div><hr/>');
-					}
-
-					$(".cancel-button").click(function(e) {
-						e.preventDefault();
-						$("#search-results").fadeOut();
-						$("#search-results").children().remove();
-						$("body").removeClass("stop-scrolling");
-					});
-
-					$(".more-info").click(function(e) {
-						e.preventDefault();
-						var link = $(this).parent().attr("href");
-						window.open(link, "_blank")
-					});
-
-					$(".addition").submit(function(e) {
-						e.preventDefault();
-						var book = {
-							"title": $(this).siblings(".book-title").html(),
-							"link": $(this).children("a").attr("href"),
-							"image": $(this).siblings(".other-information").children(".image-wrapper").children("img").attr("src"),
-							"authors": $(this).siblings(".other-information").children(".info-wrapper").children(".result-authors").children("span").html(),
-							"categories": $(this).siblings(".other-information").children(".info-wrapper").children(".result-categotries").children("span").html(),
-							"pages": $(this).siblings(".other-information").children(".info-wrapper").children(".result-pages").children("span").html(),
-							"description": $(this).siblings(".other-information").children(".info-wrapper").children(".result-description").children("span").html(),
-							"isbn": $(this).siblings(".isbn").attr("value")
-						}
-
-						$.ajax({
-							url: "/addbook",
-							type: "POST",
-							data: {_csrf: csrfToken, book: book},
-							statusCode: {
-								200: add200,
-								201: add201,
-								404: add404
-							}
-						});
-					});
-				}
+				showBooks(results);
 			},
 			error: function(error) {
 				alert(error);
@@ -110,7 +29,7 @@ $(document).ready(function() {
 			type: "POST",
 			data: {_csrf: csrfToken, isbn: isbn},
 			statusCode: {
-				201: delete201,
+				201: request200,
 				404: add404 
 			}
 		})
@@ -133,6 +52,7 @@ $(document).ready(function() {
 	});
 
 	$(".address-button").click(function() {
+		$(".address-div").children().remove();
 		var otherUser = $(this).siblings("#otherUser").attr("value");
 		$.ajax({
 			url: "/user/" + otherUser,
@@ -154,6 +74,91 @@ $(document).ready(function() {
 		})
 	});
 });
+
+function showBooks(results) {
+	if (results.totalItems == 0) {
+		alert("No results found.");
+		$("#search-results").fadeOut();
+	} else {
+		$("#search-results").children().remove();
+		if (results.items.length < 10) {
+			var limit = results.items.length;
+		} else {
+			var limit = 10;
+		}
+		for (var i = 0; i < limit; i++) {
+			var title = results.items[i].volumeInfo.title;
+			if (results.items[i].volumeInfo.subtitle) {
+				title = title + ": " + results.items[i].volumeInfo.subtitle;
+			}
+			var authors = results.items[i].volumeInfo.authors;
+			var link = results.items[i].volumeInfo.canonicalVolumeLink;
+			var categories = results.items[i].volumeInfo.categories;
+			var description = results.items[i].volumeInfo.description;
+			var image = results.items[i].volumeInfo.imageLinks.thumbnail;
+			var pages = results.items[i].volumeInfo.pageCount;
+			if (results.items[i].volumeInfo.industryIdentifiers) {
+				var isbn = results.items[i].volumeInfo.industryIdentifiers[0].identifier;
+			} else {
+				continue
+			}
+			
+			$("#search-results").append('<div class="result">' + 
+				'<input type="hidden" class="isbn" value=' + isbn + '>' +
+				'<p class="book-title">' + title + '</p>' +
+				//MUST USE FORM FOR csrfToken. When ajax uses post, server expects form data and hence csfrToken. Token passed in ajax post request below.
+				'<form class="addition" method="post" action="/addbook"><button type="submit" class="add-this-book">Add this book</button><a href=' + link + ' target="blank"><button class="generic-button more-info-button">More info</button></a><button class="cancel-button">Cancel</button></form>' +
+				'<div class="other-information">' + 
+				'<div class="image-wrapper"><img class="result-image" src=' + image + '></div>' +
+				'<div class="info-wrapper">' +
+				'<p class="result-authors"><strong>Authors:</strong> <span>' + authors + '</span></p>' +
+				'<p class="result-categotries"><strong>Categories:</strong> <span>' + categories + '</span></p>' +
+				'<p class="result-pages"><strong>Pages:</strong> <span>' + pages + '</span></p>' +
+				'<p class="result-description"><strong>Description:</strong> <span>' + description + '</span></p>' +
+				'</div>' +
+				'</div>' +
+				'</div><hr/>');
+		}
+
+		$(".cancel-button").click(function(e) {
+			e.preventDefault();
+			$("#search-results").fadeOut();
+			$("#search-results").children().remove();
+			$("body").removeClass("stop-scrolling");
+		});
+
+		$(".more-info").click(function(e) {
+			e.preventDefault();
+			var link = $(this).parent().attr("href");
+			window.open(link, "_blank")
+		});
+
+		$(".addition").submit(function(e) {
+			e.preventDefault();
+			var book = {
+				"title": $(this).siblings(".book-title").html(),
+				"link": $(this).children("a").attr("href"),
+				"image": $(this).siblings(".other-information").children(".image-wrapper").children("img").attr("src"),
+				"authors": $(this).siblings(".other-information").children(".info-wrapper").children(".result-authors").children("span").html(),
+				"categories": $(this).siblings(".other-information").children(".info-wrapper").children(".result-categotries").children("span").html(),
+				"pages": $(this).siblings(".other-information").children(".info-wrapper").children(".result-pages").children("span").html(),
+				"description": $(this).siblings(".other-information").children(".info-wrapper").children(".result-description").children("span").html(),
+				"isbn": $(this).siblings(".isbn").attr("value")
+			}
+
+			$.ajax({
+				url: "/addbook",
+				type: "POST",
+				data: {_csrf: csrfToken, book: book},
+				statusCode: {
+					200: add200,
+					201: add201,
+					404: add404
+				}
+			});
+		});
+	}
+}
 
 function add200() {
 	alert("Book is already added... Wake up my friend!");	
